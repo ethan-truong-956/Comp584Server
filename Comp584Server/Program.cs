2a2e21865f8acaf68c5ec3724960fc4f0ed6c0e8
Truong <ethan.truong.956@my.csun.edu>
2025-10-03 17:50:57 -0700

    10/3/25 Update: Database Implementation

--- Comp584Server/Program.cs
+++ Comp584Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WorldModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<Comp584DbContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddSwaggerGen(c =>
 {
