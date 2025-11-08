4968c02b98897d88566b620a00e10913658d52a8
Truong <ethan.truong.956@my.csun.edu>
2025-11-07 18:25:37 -0800

    11/7/25 Update

--- WorldModel/Comp584DbContext.cs
+++ WorldModel/Comp584DbContext.cs
@@ -33,6 +33,7 @@ public partial class Comp584DbContext : IdentityDbContext<WorldModelUser>
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<City>(entity =>
         {
             entity.HasOne(d => d.Country).WithMany(p => p.Cities)
--- WorldModel/Migrations/20251108003649_identity.Designer.cs
+++ WorldModel/Migrations/20251108003649_identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorldModel;
 
@@ -11,9 +12,11 @@ using WorldModel;
 namespace WorldModel.Migrations
 {
     [DbContext(typeof(Comp584DbContext))]
-    partial class Comp584DbContextModelSnapshot : ModelSnapshot
+    [Migration("20251108003649_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
